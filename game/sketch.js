58c3b04943755bebb7b307761ce78a39b1b1f4f4
Alexander Presthus <56009327+alexpresthus@users.noreply.github.com>
2020-06-25 22:13:58 +0200

    Update sketch.js

--- game/sketch.js
+++ game/sketch.js
@@ -113,17 +113,17 @@ let lbCell12, lbCell22, lbCell32, lbCell42, lbCell52;
 
 
 function preload () {
-  font = loadFont('galaga-game/game/assets/TurretRoad-Regular.ttf');
-  stages = loadJSON("galaga-game/game/stages.json");
-  leaderboard = loadJSON("galaga-game/game/leaderboard.json");
-  galaga_spritesheet = loadImage("galaga-game/game/img/galaga_spritesheet.png");
-  soundBullet = loadSound("galaga-game/game/sound/laser-shot.wav");
-  soundExplosion = loadSound("galaga-game/game/sound/explosion.wav");
-  soundButton = loadSound("galaga-game/game/sound/button-click.wav");
-  bgMusic = loadSound("galaga-game/game/sound/bg-music.wav");
-  soundGameOver = loadSound("galaga-game/game/sound/game-over.wav");
-  soundGameComplete = loadSound("galaga-game/game/sound/game-complete.mp3");
-  soundLifeLost = loadSound("galaga-game/game/sound/life-lost.wav");
+  font = loadFont('game/assets/TurretRoad-Regular.ttf');
+  stages = loadJSON("game/stages.json");
+  leaderboard = loadJSON("game/leaderboard.json");
+  galaga_spritesheet = loadImage("game/img/galaga_spritesheet.png");
+  soundBullet = loadSound("game/sound/laser-shot.wav");
+  soundExplosion = loadSound("game/sound/explosion.wav");
+  soundButton = loadSound("game/sound/button-click.wav");
+  bgMusic = loadSound("game/sound/bg-music.wav");
+  soundGameOver = loadSound("game/sound/game-over.wav");
+  soundGameComplete = loadSound("game/sound/game-complete.mp3");
+  soundLifeLost = loadSound("game/sound/life-lost.wav");
 }
 
 function setup () {
